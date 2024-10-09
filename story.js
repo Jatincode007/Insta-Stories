7b62db8c28ba963c216348db41c6b54c975324cc
Jatin Malik <jatinmalik9971@gmail.com>
2024-10-09 18:10:24 +0530

    story1

---
+++ story.js
@@ -0,0 +1,38 @@
+var clutter = "";
+var card=document.querySelector(".card");
+var full= document.querySelector(".fullscreen");
+var story = document.querySelector("#story");
+var stories = document.querySelector(".stories");
+var arr = [{ dp: "https://images.unsplash.com/photo-1470072508653-1be229b63562?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", story: "https://images.unsplash.com/photo-1550928431-ee0ec6db30d3?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+{ dp: "https://images.unsplash.com/photo-1498982261566-1c28c9cf4c02?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fG1vZGVsfGVufDB8fDB8fHww", story: "https://images.unsplash.com/photo-1467632499275-7a693a761056?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+{ dp: "https://images.unsplash.com/photo-1500917293891-ef795e70e1f6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", story: "https://images.unsplash.com/photo-1524504388940-b1c1722653e1?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+{ dp: "https://images.unsplash.com/photo-1512310604669-443f26c35f52?q=80&w=1934&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", story: "https://images.unsplash.com/photo-1521577352947-9bb58764b69a?q=80&w=1886&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" }
+];
+
+arr.forEach(function (e, i) {
+    clutter += `<div id="story">
+                    <img  id="${i}" src="${e.dp}">
+                </div>`;
+
+   
+
+})
+
+
+stories.innerHTML = clutter;
+stories.addEventListener("click",function(e){
+    full.style.display="block";
+    full.style.backgroundImage=`url("${arr[e.target.id].story}")`;
+    card.style.filter="blur(0px)"
+
+
+
+    setTimeout(function(){
+        full.style.display="none";
+         card.style.filter="blur(0px)"
+
+    },1200)
+})
+
+
+
